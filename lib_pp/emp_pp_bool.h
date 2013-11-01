69099f7299490982bce460eebadbebf8c13ec760
rgba8 <rgba8@free.fr>
2013-11-01 10:36:01 +0100

    lib_pp cleanup

---
+++ lib_pp/emp_pp_bool.h
@@ -0,0 +1,68 @@
+//-----------------------------------------------------------------------------
+//
+// File Name : emp_pp_bool.h
+//
+// Creation Date : Mon 11 Oct 2010 10:53:17 PM CEST
+//
+// Modification Date : ven. 01 nov. 2013 10:32:07 CET
+//
+// Created By : rgba8 (ksej) - www.empathy.fr
+//
+// Description :
+//
+//-----------------------------------------------------------------------------
+#ifndef EMP_PP_BOOL_H
+#define EMP_PP_BOOL_H
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+#include "emp_pp_token.h"
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+#define EMP_PP_BOOL(x_Value) EMP_PP_BOOL_IMP(x_Value)
+#define EMP_PP_BOOL_IMP(x_Value) EMP_PP_TOKEN(EMP_PP_BOOL_, x_Value)
+
+#define EMP_PP_BOOL_true 1
+#define EMP_PP_BOOL_false 0
+
+#define EMP_PP_BOOL_0 0
+#define EMP_PP_BOOL_1 1
+#define EMP_PP_BOOL_2 1
+#define EMP_PP_BOOL_3 1
+#define EMP_PP_BOOL_4 1
+#define EMP_PP_BOOL_5 1
+#define EMP_PP_BOOL_6 1
+#define EMP_PP_BOOL_7 1
+#define EMP_PP_BOOL_8 1
+#define EMP_PP_BOOL_9 1
+#define EMP_PP_BOOL_10 1
+#define EMP_PP_BOOL_11 1
+#define EMP_PP_BOOL_12 1
+#define EMP_PP_BOOL_13 1
+#define EMP_PP_BOOL_14 1
+#define EMP_PP_BOOL_15 1
+#define EMP_PP_BOOL_16 1
+#define EMP_PP_BOOL_17 1
+#define EMP_PP_BOOL_18 1
+#define EMP_PP_BOOL_19 1
+#define EMP_PP_BOOL_20 1
+#define EMP_PP_BOOL_21 1
+#define EMP_PP_BOOL_22 1
+#define EMP_PP_BOOL_23 1
+#define EMP_PP_BOOL_24 1
+#define EMP_PP_BOOL_25 1
+#define EMP_PP_BOOL_26 1
+#define EMP_PP_BOOL_27 1
+#define EMP_PP_BOOL_28 1
+#define EMP_PP_BOOL_29 1
+#define EMP_PP_BOOL_30 1
+#define EMP_PP_BOOL_31 1
+#define EMP_PP_BOOL_32 1
+#define EMP_PP_BOOL_33 1
+#define EMP_PP_BOOL_34 EMP_PP_ERROR_BOOL_OVERFLOW
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+#endif
+
