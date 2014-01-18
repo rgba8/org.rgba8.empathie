36cd327f8953b7f461246380daaabf8655dd7ba7
rgba8 <rgba8@free.fr>
2014-01-18 13:04:46 +0100

    make check

--- lib_pp/emp_pp_if_not.h
+++ lib_pp/emp_pp_if_not.h
@@ -4,7 +4,7 @@
 //
 // Creation Date : Mon 11 Oct 2010 10:53:25 PM CEST
 //
-// Modification Date : Sun Jan 12 23:31:59 2014
+// Modification Date : Sat Jan 18 12:33:29 2014
 //
 // Created By : luh - www.rgba8.org
 //
@@ -21,7 +21,7 @@
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-#define EMP_PP_IF_NOT(x_Condition, x_Then) EMP_PP_IF_NOT_IMP(x_Condition, x_Then)
+#define EMP_PP_IF_NOT(x_Condition,x_Then) EMP_PP_IF_NOT_IMP(x_Condition,x_Then)
 #define EMP_PP_IF_NOT_IMP(x_Condition, x_Then)\
     EMP_PP_TOKEN(EMP_PP_IF_NOT_IMP_, EMP_PP_BOOL(x_Condition))(x_Then)
 
